f7c3e8cef9da6b0534b7f6e4ea2574ddc24ac0c7
Vladimir <vladimir.papin@gmail.com>
2023-05-12 10:39:04 +0300

    moved parse book page to a separate function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os.path
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urlparse, urlsplit
+from urllib.parse import urljoin, urlparse
 
 
 def check_for_redirect(response):
@@ -46,48 +46,13 @@ def download_txt(url, filename, folder='books/'):
     return file_path
 
 
-def get_book_link_and_name(url):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    check_for_redirect(response)
-
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    book_comment_tags = soup.find('div', id='content').find_all('div', class_='texts')
-    book_comments = []
-    for book_comment_tag in book_comment_tags:
-        book_comments.append(book_comment_tag.find('span', class_='black').text)
-
-    book_genre_tags = soup.find('div', id='content').find('span', class_='d_book').find_all('a')
-    book_genres = []
-    for book_genre_tag in book_genre_tags:
-        book_genres.append(book_genre_tag.text)
-
-    book_img_shot_url = soup.find('div', id='content').find('img')['src']
-    book_name_tag = soup.find('div', id='content').find('h1')
-    book_name = book_name_tag.text.split('::')[0].strip()
-    author_name = book_name_tag.text.split('::')[-1].strip()
-    try:
-        book_urlpath = soup.find_all('a', string='скачать txt')[0].get('href')
-    except IndexError:
-        return None
-
-    url_components = urlsplit(url)
-
-    book_url_components = url_components._replace(path=book_urlpath)
-    book_img_url_components = url_components._replace(path=book_img_shot_url)
-
-    book_txt_url = book_url_components.geturl()
-    book_img_url = book_img_url_components.geturl()
-
-    return (book_txt_url, book_name, book_img_url, book_comments, book_genres)
-
-
 def save_image_from_url(url):
     image_path = 'images'
     Path(image_path).mkdir(parents=True, exist_ok=True)
-    image_file_name = os.path.join(image_path, os.path.split(urlparse(url).path)[-1])
+    image_file_name = os.path.join(
+        image_path,
+        os.path.split(urlparse(url).path)[-1]
+    )
 
     response = requests.get(url)
     response.raise_for_status()
@@ -96,23 +61,79 @@ def save_image_from_url(url):
         file.write(response.content)
 
 
-def main():
-    template_url = 'https://tululu.org/b{}/'
-    for id_book in range(1, 11):
-        try:
-            book_link_and_name = get_book_link_and_name(template_url.format(id_book))
-            if book_link_and_name:
-                book_txt_url = book_link_and_name[0]
-                book_name = book_link_and_name[1]
-                book_img_url = book_link_and_name[2]
-                book_comments = book_link_and_name[3]
-                book_genres = book_link_and_name[4]
-                print(f'{book_name}\n{book_genres}\n{book_comments}')
+def parse_book_page(html_book_page):
+    soup = BeautifulSoup(html_book_page, 'lxml')
 
-                # download_txt(book_txt_url, book_name)
-                # save_image_from_url(book_img_url)
+    book_comment_tags = soup.find(
+        'div', id='content').find_all(
+        'div', class_='texts'
+        )
+    book_comments = []
+    for book_comment_tag in book_comment_tags:
+        book_comments.append(
+            book_comment_tag.find('span', class_='black').text
+        )
+
+    book_genre_tags = soup.find(
+        'div', id='content').find(
+        'span', class_='d_book').find_all('a')
+    book_genres = []
+    for book_genre_tag in book_genre_tags:
+        book_genres.append(book_genre_tag.text)
+
+    book_img_shot_url = soup.find('div', id='content').find('img')['src']
+    book_name_tag = soup.find('div', id='content').find('h1')
+    book_name = book_name_tag.text.split('::')[0].strip()
+    book_author = book_name_tag.text.split('::')[-1].strip()
+
+    try:
+        book_txt_short_url = soup.find_all(
+            'a', string='скачать txt')[0].get('href')
+    except IndexError:
+        return None
+
+    book_page_metadata = {
+        'book_txt_short_url': book_txt_short_url,
+        'book_name': book_name,
+        'book_author': book_author,
+        'book_img_shot_url': book_img_shot_url,
+        'book_comments': book_comments,
+        'book_genres': book_genres,
+    }
+    return book_page_metadata
+
+
+def main():
+    base_url = 'https://tululu.org/'
+    path_url = 'b{}/'
+    for id_book in range(1, 11):
+        book_url = urljoin(base_url, path_url.format(id_book))
+        response = requests.get(book_url)
+        response.raise_for_status()
+
+        try:
+            check_for_redirect(response)
         except HTTPError:
             continue
+
+        book_page_metadata = parse_book_page(response.text)
+
+        if book_page_metadata:
+            book_txt_url = urljoin(
+                base_url,
+                book_page_metadata['book_txt_short_url']
+            )
+            book_name = book_page_metadata['book_name']
+            book_img_url = urljoin(
+                base_url,
+                book_page_metadata['book_img_shot_url']
+            )
+            # book_comments = book_page_metadata['book_comments']
+            # book_genres = book_page_metadata['book_genres']
+            # book_author = book_page_metadata['book_author']
+            download_txt(book_txt_url, book_name)
+            save_image_from_url(book_img_url)
+        continue
 
 
 if __name__ == '__main__':
