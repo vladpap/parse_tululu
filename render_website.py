d9d92023416e440afe25736c8fc9421cbe92563f
Vladimir Papin <vladimir.papin@gmail.com>
2023-07-11 20:02:27 +0300

    Split books by two columns

--- render_website.py
+++ render_website.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server
+from more_itertools import chunked
 
 
 def load_books():
@@ -21,14 +22,11 @@ def rebuild():
 
     template = env.get_template('template.html')
     rendered_page = template.render(
-        books=books,
+        books=list(chunked(books, 2)),
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
-
-    #server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    #server.serve_forever()
 
 
 def main():
